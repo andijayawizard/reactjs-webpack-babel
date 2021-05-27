3ac3685a78a742cf164112a2241af6d055714bde
andijayawizard <33024329+andijayawizard@users.noreply.github.com>
2021-05-27 16:38:19 +0700

    2705

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const HelloWorld = () => {
+  return <h1>halo dunia, the jungle</h1>;
+};
+ReactDom.render(<HelloWorld />, document.getElementById("root"));
